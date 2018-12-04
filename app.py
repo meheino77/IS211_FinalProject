ef5557464dc5c48f151c79f5357e503b4e9053ff
meheino77 <35932509+meheino77@users.noreply.github.com>
2018-12-04 13:23:59 -0500

    login and index formatted

--- app.py
+++ app.py
@@ -6,16 +6,18 @@ Created on Tue Dec  4 00:20:06 2018
 @author: ntcrwlr77
 """
 
-from flask import render_template, request, flash
+from flask import render_template, request, flash, redirect, url_for
 
 from config import app
 from db_setup import init_db, insert_user, insert_users 
 from forms import LoginForm
+from models import User
 
 
 
 @app.route('/', methods=['GET','POST'])
-def main():
+@app.route('/index', methods=['GET','POST'])
+def index():
     #return "Welcome"
     
     form = LoginForm()
@@ -24,19 +26,31 @@ def main():
         return render_template('login.html', form=form)
     else: 
         if form.validate_on_submit():
-            data = form.username.data
-            return "POST" + data
-
-
-@app.route('/index')
-def index():
+            
+            isthere = User.query.filter_by(email=form.username.data, password=form.password.data).first()
     
-    return "IN INDEX"
+            if isthere is not None:
+                #return "POST" + isthere.email + " " + isthere.password
+                return redirect(url_for('main_page'))
+            else:
+                return "User not found"
+                
 
+
+@app.route('/main_page', methods=['GET','POST'])
+def main_page():
+    
+   # return "IN INDEX"
+    return render_template('dashboard.html')
+
+def intialize_database():
+    
+    init_db()
+    insert_users()
+    insert_user("gpheino@msn.com", "simple")
+    
 if __name__ == '__main__':
     
-    #init_db()
-    #insert_users()
-    #insert_user("gpheino@msn.com", "simple")
+    intialize_database()
     
-    app.run(debug=True)
+    app.run(debug=False)
