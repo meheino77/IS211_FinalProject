f5a954c3246c26c7d4f47dfe0a1ae08b5e6c450c
meheino77 <35932509+meheino77@users.noreply.github.com>
2018-12-05 00:56:25 -0500

    Some form updates

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Created on Tue Dec  4 00:20:06 2018
 @author: ntcrwlr77
 """
 
-from flask import render_template, request, flash, redirect, url_for
+from flask import render_template, request, flash, redirect, url_for, session
 
 from config import app
 from db_setup import init_db, insert_user, insert_users 
@@ -18,7 +18,6 @@ from models import User
 @app.route('/', methods=['GET','POST'])
 @app.route('/index', methods=['GET','POST'])
 def index():
-    #return "Welcome"
     
     form = LoginForm()
     
@@ -30,18 +29,48 @@ def index():
             isthere = User.query.filter_by(email=form.username.data, password=form.password.data).first()
     
             if isthere is not None:
+                
                 #return "POST" + isthere.email + " " + isthere.password
+                session["user_id"] = isthere.id
+                session["logged_in"] = True
                 return redirect(url_for('main_page'))
             else:
                 return "User not found"
                 
-
+@app.route('/log_out',methods=['GET','POST'])
+def log_out():
+    
+    session.pop('logged_in', None)
+    session.pop('user_id', None)
+    
+    return redirect(url_for('index'))
+    
 
 @app.route('/main_page', methods=['GET','POST'])
 def main_page():
     
-   # return "IN INDEX"
+    flash("You are logged in! ")
     return render_template('dashboard.html')
+
+
+@app.route('/view_contact', methods=['GET','POST'])
+def view_contact():
+    
+   # return "Session ID: " + str(session["user_id"]) 
+    
+    
+   if request.method == "GET":
+       ses = str(session['user_id']) 
+       yorn = session['logged_in']
+       
+       #isContact
+       flash("You Are seeing a get request")
+       return render_template ('view_contact.html', sess=ses, logged_in = yorn,
+                               name='Matthew Heino', address="126 Popple Bridge Rd",
+                               city='Griswold', state='CT')
+       
+       #n render_template ('view_contact.html')
+
 
 def intialize_database():
     
@@ -53,4 +82,4 @@ if __name__ == '__main__':
     
     intialize_database()
     
-    app.run(debug=False)
+    app.run(debug=True)
--- forms.py
+++ forms.py
@@ -7,13 +7,14 @@ Created on Tue Dec  4 06:24:22 2018
 """
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
     
     username = StringField('Username', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
+    #password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log In')
     
